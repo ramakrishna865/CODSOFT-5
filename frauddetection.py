9e8408cae4a5c194dae1451a4acf6399f0420f80
ramakrishna865 <136302037+ramakrishna865@users.noreply.github.com>
2023-09-05 16:24:24 +0530

    Create frauddetection.py

---
+++ frauddetection.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from imblearn.over_sampling import RandomOverSampler
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.metrics import classification_report_imbalanced
+from collections import Counter
+
+
+data = pd.read_csv("C:\\Users\\Rama Krishna\\Desktop\\fraud\\creditcard.csv")
+
+# Explore the dataset
+print(data.head())
+
+# Check class balance
+class_counts = data['Class'].value_counts()
+print(class_counts)
+
+# Visualize class distribution
+plt.figure(figsize=(8, 4))
+sns.countplot(x='Class', data=data)
+plt.title("Class Distribution")
+plt.show()
+
+# Data preprocessing
+X = data.drop('Class', axis=1)
+y = data['Class']
+
+# Normalize the 'Amount' feature
+scaler = StandardScaler()
+X['Amount'] = scaler.fit_transform(X['Amount'].values.reshape(-1, 1))
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Handle class imbalance (you can choose either oversampling or undersampling)
+oversampler = RandomOverSampler(sampling_strategy=0.5, random_state=42)
+X_train_resampled, y_train_resampled = oversampler.fit_resample(X_train, y_train)
+
+# Train a Random Forest Classifier
+clf = RandomForestClassifier(random_state=42)
+clf.fit(X_train_resampled, y_train_resampled)
+
+# Evaluate the model
+y_pred = clf.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred))
+
+# Visualize the confusion matrix
+plt.figure(figsize=(8, 6))
+sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, fmt="d", cmap="Blues")
+plt.xlabel('Predicted Labels')
+plt.ylabel('True Labels')
+plt.title('Confusion Matrix')
+plt.show()
+
+# Visualize feature importance (if using Random Forest)
+feature_importance = clf.feature_importances_
+feature_names = X.columns
+plt.figure(figsize=(10, 6))
+sns.barplot(x=feature_importance, y=feature_names)
+plt.title("Feature Importance")
+plt.xticks(rotation=90)
+plt.show()
+
+# Sort feature importances in descending order
+sorted_idx = np.argsort(feature_importance)[::-1]
+sorted_feature_importance = feature_importance[sorted_idx]
+sorted_feature_names = feature_names[sorted_idx]
+
+# Create a bar chart
+plt.figure(figsize=(12, 6))
+sns.barplot(x=sorted_feature_importance, y=sorted_feature_names, palette='viridis')
+plt.xlabel('Feature Importance')
+plt.ylabel('Features')
+plt.title('Feature Importance')
+plt.show()
